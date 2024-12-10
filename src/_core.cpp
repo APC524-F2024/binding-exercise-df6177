96838291189b486ae318cb335fc253cb43cf80eb
denisfaer <74679045+denisfaer@users.noreply.github.com>
2024-12-10 15:43:37 -0500

    Updated _core.cpp

--- src/_core.cpp
+++ src/_core.cpp
@@ -7,5 +7,9 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
-  /// ... Fill me in!
+    py::class_<HarmonicOscillator>(m, "HarmonicOscillator")
+        .def(py::init<>())
+        .def("f", &HarmonicOscillator::f, py::arg("t"), py::arg("y"))
+        .def("compute_step", &HarmonicOscillator::compute_step, py::arg("t_n"), py::arg("y_n"), py::arg("h"))
+        .def("integrate", &HarmonicOscillator::integrate, py::arg("stepsize"), py::arg("steps"), py::arg("init_y"));
 }
